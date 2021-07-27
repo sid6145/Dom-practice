b7ed493f36dbf5e0cdfdc5fd0bee12be455ff42f
sid6145 <sidtdesh@gmail.com>
2021-07-27 16:33:53 +0530

    implemented color change on button click and also added styling to buttons

--- dom1/script.js
+++ dom1/script.js
@@ -3,19 +3,31 @@ let green = document.getElementById('green')
 let blue = document.getElementById('blue')
 let yellow = document.getElementById('yellow')
 let orange = document.getElementById('orange')
+let main = document.querySelector('.main')
+let previous = 'red'
 
 red.addEventListener('click', function(){
-    document.querySelector('.main').classList.toggle('red')
+    main.classList.remove(previous)
+    main.classList.add('red')
+    previous = 'red'
 })
 green.addEventListener('click', function(){
-    document.querySelector('.main').classList.toggle('green')
+    main.classList.remove(previous)
+    main.classList.add('green')
+    previous = 'green'
 })
 blue.addEventListener('click', function(){  
- document.querySelector('.main').classList.toggle('blue')
+    main.classList.remove(previous)
+    main.classList.add('blue')
+    previous = 'blue'
 })
 yellow.addEventListener('click', function(){
-    document.querySelector('.main').classList.toggle('yellow')
+    main.classList.remove(previous)
+    main.classList.add('yellow')
+    previous = 'yellow'
 })
 orange.addEventListener('click', function(){
-    document.querySelector('.main').classList.toggle('orange')
+    main.classList.remove(previous)
+    main.classList.add('orange')
+    previous = 'orange'
 })
