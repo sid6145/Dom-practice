dd60daba333ccbd63080a83c880a89c810d98f4b
sid6145 <sidtdesh@gmail.com>
2021-07-27 16:02:05 +0530

    added script to change color based on which button is pressed

---
+++ dom1/script.js
@@ -0,0 +1,21 @@
+let red = document.getElementById('red')
+let green = document.getElementById('green')
+let blue = document.getElementById('blue')
+let yellow = document.getElementById('yellow')
+let orange = document.getElementById('orange')
+
+red.addEventListener('click', function(){
+    document.querySelector('.main').classList.toggle('red')
+})
+green.addEventListener('click', function(){
+    document.querySelector('.main').classList.toggle('green')
+})
+blue.addEventListener('click', function(){  
+ document.querySelector('.main').classList.add('blue')
+})
+yellow.addEventListener('click', function(){
+    document.querySelector('.main').classList.add('yellow')
+})
+orange.addEventListener('click', function(){
+    document.querySelector('.main').classList.add('orange')
+})
