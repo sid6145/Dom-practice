4b0dd4509f163be480f029513f97a4b100ef1168
sid6145 <sidtdesh@gmail.com>
2021-07-27 20:13:31 +0530

    item can now be dragged from one place to another

--- dom2/script.js
+++ dom2/script.js
@@ -1,7 +1,7 @@
 
 
 document.ondragstart = function (event){
-    event.dataTransfer.setData("Text", event.target.id)
+    event.dataTransfer.setData("item", event.target.id)
 }
 
 document.ondrag = function(event){
@@ -15,7 +15,7 @@ document.ondragover = function(event){
 document.ondrop = function(event){
     event.preventDefault()
     if(event.target.id == "dropZone"){
-        var data = event.dataTransfer.getData("Text");
+        var data = event.dataTransfer.getData("item");
         event.target.appendChild(document.getElementById(data));
         document.getElementById('dragText').innerHTML = "oh i am alive"
     }
