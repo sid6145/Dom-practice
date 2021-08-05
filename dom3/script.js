4c870f8b8938cfa9446a98bbe3d8350b885a4b72
sid6145 <sidtdesh@gmail.com>
2021-08-05 15:40:24 +0530

    initial layout and styling

---
+++ dom3/script.js
@@ -0,0 +1,16 @@
+// document.addEventListener('keypress', (e) => {
+//     makeSound(e.key)
+// })
+
+// function makeSound(key) {
+//     switch(key){
+//         case "a":
+//             let tom1 = new Audio("sounds/tom-1.mp3");
+//             tom1.play();
+//             break;
+        
+
+//         default:
+//             console.log(key)
+//     }
+// }
