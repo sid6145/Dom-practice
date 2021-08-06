9f3631c06cc7ef9fbc700959e54c86438c5e2df5
sid6145 <sidtdesh@gmail.com>
2021-08-06 14:02:34 +0530

    added logic for minute and hour hand

--- dom4/script.js
+++ dom4/script.js
@@ -1,10 +1,20 @@
 const secondHand = document.querySelector('.second-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const hourHand = document.querySelector('.hour-hand')
 function setDate(){
     const now = new Date
     const seconds = now.getSeconds()
     const secondsDegree = (((seconds / 60) * 360) + 90)
     secondHand.style.transform = `rotate(${secondsDegree}deg)`
     console.log(seconds)
+
+    const minutes = now.getMinutes()
+    const minutesDegree = (((minutes / 60) * 360) + 90)
+    minuteHand.style.transform = `rotate(${minutesDegree}deg)`
+
+    const hour = now.getHours()
+    const hourDegree = (((min / 12) * 360) + 90)
+    hourHand.style.transform = `rotate(${hourDegree}deg)`
 }
 
 setInterval(setDate, 1000)
