81c5b894433014aa982720e96b539e53d6cd5c0e
sid6145 <sidtdesh@gmail.com>
2021-08-07 14:47:34 +0530

    added animation when hambuger icon is clicked

--- dom5/script.js
+++ dom5/script.js
@@ -1,8 +1,11 @@
 let burger = document.querySelector('.burger')
 let nav = document.querySelector('.nav-items')
 
+
 burger.addEventListener('click', function(){
    nav.classList.toggle('nav-active')
+
+   burger.classList.toggle('toggle');
 })
 
 
